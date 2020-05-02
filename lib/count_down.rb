37e4515a4f9c1879d230c7731d5373e318b7b652
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-02 04:47:12 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,8 @@
 count = 11 
-while count <= 11 do 
+while count < 11 do 
   break if count == 0 
   puts " #{count}" 
   count = count - 1 
   end
+  count ==0 
   puts "Happy New Year!"# Write your code here
